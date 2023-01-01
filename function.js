c2d0a22a77ae82c3742c6988b67cea23e498d060
hipdz21 <quangdo120701@gmail.com~>
2023-01-01 17:58:34 +0700

    update code javascript

--- function.js
+++ function.js
@@ -1,99 +1,134 @@
-//links
-//http://eloquentjavascript.net/09_regexp.html
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-
-var messages = [], //array that hold the record of each string in chat
-  lastUserMessage = "", //keeps track of the most recent input string from the user
-  botMessage = "", //var keeps track of what the chatbot is going to say
-  botName = "Chatbot", //name of the chatbot
-  talking = true, //when false the speach function doesn't work
+var messages = [],
+  lastUserMessage = "",
+  botMessage = "",
+  botName = "Chuyên gia",
   seconUserMessage = "";
-//
-//
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//edit this function to change what the chatbot says
-function chatbotResponse() {
-  talking = true;
-  botMessage = "I'm confused"; //the default message
 
-  if (lastUserMessage === "hi" || lastUserMessage == "hello") {
-    const hi = ["hi", "howdy", "hello"];
-    botMessage = hi[Math.floor(Math.random() * hi.length)];
-  }
-
-  if (lastUserMessage === "name") {
-    botMessage = "My name is " + botName;
-  }
-
-  if (lastUserMessage === "nghia") {
-    if (lastUserMessage === "tuoi") {
-      botMessage = "18";
-    }
-  }
+function chatbotStart() {
+  chatbotResponse(
+    "Xin chào bạn! Bạn muốn làm gì?" +
+      "<br>1. Chẩn đoán bệnh tiêu hoá." +
+      "<br>2. làm gì đó." +
+      "<br>3. Làm gì đó." +
+      "<br><i>(Vui lòng nhập các lựa chọn bằng số)</i>"
+  );
 }
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//
-//
-//
-//this runs each time enter is pressed.
-//It controls the overall input and output
+function chatbotEnd() {
+  chatbotResponse("Cảm ơn bạn đã sử dụng hệ thống!"+
+  "<br>Tạm biệt bạn! Chúc bạn một ngày tốt lành.")
+}
+
+function chatbotResponse(text = "") {
+  botMessage = "Xin lỗi, tôi không thể thực hiện chức năng này!";
+  if (text != "") {
+    botMessage = text;
+  }
+  messages.push("<b>" + botName + ":</b> " + botMessage);
+  showMessage();
+}
+
+function getMessage() {
+  lastUserMessage = document.getElementById("chatbox").value;
+  document.getElementById("chatbox").value = "";
+  messages.push(lastUserMessage);
+}
+
+function showMessage() {
+  var html = "";
+  for (var i = 0; i < messages.length; i++) {
+    html += '<p class="chatlog">' + messages[i] + "</p>";
+  }
+  document.getElementById("chatResponse").innerHTML = html;
+  var chatHistory = document.getElementById("chatResponse");
+  chatHistory.scrollTop = chatHistory.scrollHeight;
+}
+var check;
 function newEntry() {
-  //if the message from the user isn't empty then run
-  if (document.getElementById("chatbox").value != "") {
-    //pulls the value from the chatbox ands sets it to lastUserMessage
-    lastUserMessage = document.getElementById("chatbox").value;
-    //sets the chat box to be clear
-    document.getElementById("chatbox").value = "";
-    //adds the value of the chatbox to the array messages
-    messages.push(lastUserMessage);
-    //Speech(lastUserMessage);  //says what the user typed outloud
-    //sets the variable botMessage in response to lastUserMessage
-    chatbotResponse();
-    //add the chatbot's name and message to the array messages
-    messages.push("<b>" + botName + ":</b> " + botMessage);
-    // says the message using the text to speech function written below
-    //Speech(botMessage);
-    //outputs the last few array elements of messages to html
-    var html = "";
-    for (var i = 0; i < messages.length; i++) {
-      html += '<p class="chatlog">' + messages[i] + "</p>";
+  chatbotStart();
+  document.getElementById("chatbox").onkeydown = function (e) {
+    if (e.keyCode == 13) {
+      if (document.getElementById("chatbox").value != "") {
+        getMessage();
+        if (lastUserMessage == "1") {
+          KB1();
+        } else if (lastUserMessage == "2") {
+          KB2();
+        } else if (lastUserMessage == "3") {
+          KB3();
+        } else {
+          chatbotResponse();
+        }
+      }
     }
-    document.getElementById("chatResponse").innerHTML = html;
-    var chatHistory = document.getElementById("chatResponse");
-    chatHistory.scrollTop = chatHistory.scrollHeight;
-  }
+  };
 }
 
-//runs the keypress() function when a key is pressed
-document.onkeypress = keyPress;
-//if the key pressed is 'enter' runs the function newEntry()
-function keyPress(e) {
-  var x = e || window.event;
-  var key = x.keyCode || x.which;
-  if (key == 13 || key == 3) {
-    //runs this function when enter is pressed
-    newEntry();
-  }
-  if (key == 38) {
-    console.log("hi");
-    //document.getElementById("chatbox").value = lastUserMessage;
-  }
+function KB1() {
+  chatbotResponse(
+    "Vui lòng nhập các triệu chứng bạn đang gặp phải!" +
+      '<br><i>(Các triệu chứng ngăn cách nhau bằng dấu phẩy ",")'
+  );
+  document.getElementById("chatbox").onkeydown = function (e) {
+    if (e.keyCode == 13) {
+      if (document.getElementById("chatbox").value != "") {
+        getMessage()
+        let listTrieuChung = [];
+        let txtArray = lastUserMessage.split(",");
+        for (let i of txtArray) {
+          i = i.trim();
+          if (i != "" && !listTrieuChung.includes(i)) {
+            listTrieuChung.push(i);
+          }
+        }
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify(listTrieuChung);
+
+        var requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: raw,
+          redirect: "follow",
+        };
+
+        fetch("http://localhost:8080/api/cbr-benh", requestOptions)
+          .then((response) => response.json())
+          .then((result) => {
+            if(result.value != '0'){
+              chatbotResponse("Có thể bạn đang bị bệnh <b>"+result.message+"</b>");
+            } else {
+              chatbotResponse("Không thể chẩn đoán bệnh của bạn, bạn cần đến trung tâm y tế để kiểm tra cũng như làm các xét nghiệm liên quan.")
+            }
+            chatbotResponse("Bạn có muốn thực hiện chức năng khác?")
+            document.getElementById("chatbox").onkeydown = function (e) {
+              if (e.keyCode == 13) {
+                if (document.getElementById("chatbox").value != "") {
+                  getMessage();
+                  var dongY = ['có', 'co', 'c', 'yes', 'y', 'đồng ý', 'ok', 'yup']
+                  if(dongY.includes(lastUserMessage.toLowerCase())){
+                    newEntry();
+                  } else {
+                    chatbotEnd();
+                  }
+                }
+              }
+            };
+          })
+          .catch((error) => console.log("error", error));
+      }
+    }
+  };
+}
+function KB2() {
+  chatbotResponse("Chưa phát triển!");
+}
+function KB3() {
+  chatbotResponse("Chưa phát triển!");
 }
 
-//clears the placeholder text ion the chatbox
-//this function is set to run when the users brings focus to the chatbox, by clicking on it
 function placeHolder() {
   document.getElementById("chatbox").placeholder = "";
 }
+
+newEntry();
